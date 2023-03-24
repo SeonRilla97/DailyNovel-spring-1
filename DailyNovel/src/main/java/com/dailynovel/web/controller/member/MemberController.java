e08918cfebaae37b73c0b1ae76c6aa7c4fdda139
SeonRilla <SeonRilla97@gmail.com>
2023-03-24 10:50:19 +0900

    NEW(signup) : 새로이 시작하는 나만의 사인업 공간 Seonrilla

--- DailyNovel/src/main/java/com/dailynovel/web/controller/member/MemberController.java
+++ DailyNovel/src/main/java/com/dailynovel/web/controller/member/MemberController.java
@@ -3,8 +3,8 @@ package com.dailynovel.web.controller.member;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/member/")
 @Controller
+@RequestMapping("/member/")
 public class  MemberController {
 
 	
@@ -12,13 +12,13 @@ public class  MemberController {
 	@RequestMapping("main")
 	public String main(){
 
-		return "/WEB-INF/view/member/main.jsp";
+		return "/member/main";
 	}
 	
 
 	@RequestMapping("setting")
 	public String setting(){
-		return "/member/settings/main.html";
+		return "/member/settings/main";
 	}
 	
 	
