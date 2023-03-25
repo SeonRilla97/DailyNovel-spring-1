ad876b598a53ba2d803e2067cb52b330fcf4e606
DongDong107 <jkds989@gmail.com>
2023-03-26 02:12:43 +0900

    create register branch and test

--- DailyNovel/src/main/java/com/dailynovel/web/controller/member/MemberController.java
+++ DailyNovel/src/main/java/com/dailynovel/web/controller/member/MemberController.java
@@ -3,8 +3,8 @@ package com.dailynovel.web.controller.member;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/member/")
 @Controller
+@RequestMapping("/member/")
 public class  MemberController {
 
 	
@@ -13,15 +13,17 @@ public class  MemberController {
 	public String main(){
 
 		return "/WEB-INF/view/member/main.jsp";
-	}
-	
+	}	
 
 	@RequestMapping("setting")
 	public String setting(){
 		return "/member/settings/main.html";
 	}
 	
-	
+	@RequestMapping("register")
+	public String register() {
+		return "/member/diary/register";
+	}
 
 	
 }
