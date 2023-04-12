0acff7e6fc903ecb474b483d4117ede1b2cb8b6b
DongDong107 <jkds989@gmail.com>
2023-04-12 23:02:40 +0900

    Feat,Design(register) : 날씨 디자인 변경 및 오류 수정

--- DailyNovel/src/main/java/com/dailynovel/web/controller/user/accountRecovery/AccountRecoveryController.java
+++ DailyNovel/src/main/java/com/dailynovel/web/controller/user/accountRecovery/AccountRecoveryController.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.dailynovel.web.controller.member.test;
+
 import com.dailynovel.web.service.MemberService;
 
 import jakarta.mail.internet.MimeMessage;
